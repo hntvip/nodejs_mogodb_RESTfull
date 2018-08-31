94e31d3e009f940d2ae8b552d8ec69904db7b894
Tai Phan <tai101568@gmail.com>
2018-08-31 15:38:09 +0700

    fisrt commit

---
+++ demo_mongodb_find_fields.js
@@ -0,0 +1,13 @@
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url,{ useNewUrlParser: true }, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.collection("customers").find({}, { name: 1, address: 0 }).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
